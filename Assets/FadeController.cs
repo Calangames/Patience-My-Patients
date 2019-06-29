4b227930b797437948a4028f4490bdf42c3b95fa
wesleyronald <wesleyronaldma@gmail.com>
2019-06-29 11:51:09 -0300

    Between Scenes fade
    Needs more organizing lol

---
+++ Assets/FadeController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeController : MonoBehaviour 
+{
+    public static FadeController instance = null;
+
+    private bool white = false, black = false;
+
+    public bool White()
+    {
+        return white;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void White(bool value)
+    {
+        white = value;
+    }
+
+    public bool Black()
+    {
+        return black;
+    }
+
+    public void Black(bool value)
+    {
+        black = value;
+    }
+}
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,13 +7,26 @@ using UnityEngine.EventSystems;
 
 public class MenuController : MonoBehaviour
 {
+    [Range(0.1f, 1f)]
+    public float fadeDuration = 0.5f;
+    [Range(0.1f, 4f)]
+    public float whiteFadeDuration = 2.5f;
     public Button creditsButton, startButton, exitButton;
     public GameObject credits;
+    public Image fade;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        if (FadeController.instance.Black())
+        {
+            StartCoroutine(FadeFromBlack());
+        }
+        else if (FadeController.instance.White())
+        {
+            StartCoroutine(FadeFromWhite());
+        }
     }
 
     void Update()
@@ -30,6 +43,10 @@ public class MenuController : MonoBehaviour
                 startButton.Select();
             }
         }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            exitButton.Select();
+        }
     }
 
     public void CreditsButton()
@@ -41,7 +58,7 @@ public class MenuController : MonoBehaviour
     public void StartButton()
     {
         SoundController.instance.Crossfade(SoundController.instance.menu, SoundController.instance.game);
-        SceneManager.LoadScene(1);
+        StartCoroutine(FadeToBlackAndStart());
     }
 
     public void ExitButton()
@@ -53,6 +70,69 @@ public class MenuController : MonoBehaviour
     {
         Navigation nav = button.navigation;
         nav.mode = nav.mode == Navigation.Mode.Automatic ? Navigation.Mode.None : Navigation.Mode.Automatic;
-        creditsButton.navigation = nav;
+        button.navigation = nav;
+    }
+
+    private IEnumerator FadeFromBlack()
+    {
+        EventSystem.current.sendNavigationEvents = false;
+        SwapNavigation(startButton);
+        SwapNavigation(exitButton);
+        SwapNavigation(creditsButton);
+        fade.color = Color.black;
+        yield return null;
+        float alpha = 1f;
+        while (alpha > 0f)
+        {
+            alpha -= (Time.deltaTime / fadeDuration);
+            fade.color = new Color(0f, 0f, 0f, alpha);
+            yield return null;
+        }
+        FadeController.instance.Black(false);
+        SwapNavigation(startButton);
+        SwapNavigation(exitButton);
+        SwapNavigation(creditsButton);
+        EventSystem.current.sendNavigationEvents = true;
+    }
+
+    private IEnumerator FadeFromWhite()
+    {
+        EventSystem.current.sendNavigationEvents = false;
+        SwapNavigation(startButton);
+        SwapNavigation(exitButton);
+        SwapNavigation(creditsButton);
+        fade.color = Color.white;
+        yield return null;
+        float alpha = 1f;
+        while (alpha > 0f)
+        {
+            alpha -= (Time.deltaTime / whiteFadeDuration);
+            fade.color = new Color(1f, 1f, 1f, alpha);
+            yield return null;
+        }
+        FadeController.instance.White(false);
+        SwapNavigation(startButton);
+        SwapNavigation(exitButton);
+        SwapNavigation(creditsButton);
+        EventSystem.current.sendNavigationEvents = true;
+    }
+
+    private IEnumerator FadeToBlackAndStart()
+    {
+        EventSystem.current.sendNavigationEvents = false;
+        SwapNavigation(startButton);
+        SwapNavigation(exitButton);
+        SwapNavigation(creditsButton);
+        fade.color = new Color(0f, 0f, 0f, 0f);
+        yield return null;
+        float alpha = 0f;
+        while (alpha < 1f)
+        {
+            alpha += (Time.deltaTime / fadeDuration);
+            fade.color = new Color(0f, 0f, 0f, alpha);
+            yield return null;
+        }
+        FadeController.instance.Black(true);
+        SceneManager.LoadScene(1);
     }
 }
